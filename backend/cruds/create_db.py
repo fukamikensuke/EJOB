1e5402529be81267a25b73936773e461f7a5e0b3
Kensuke Fukami <kensuke8293@gmail.com>
2022-12-03 18:39:38 +0900

    fix: table_name

--- backend/cruds/create_db.py
+++ backend/cruds/create_db.py
@@ -33,11 +33,11 @@ cur = cnxn.cursor()
 # cur.execute("DROP TABLE IF EXISTS Season")
 
 
-# cur.execute("CREATE TABLE Intern_detail(id INT IDENTITY PRIMARY KEY,company_name NVARCHAR(128) NOT NULL,period_id INT NOT NULL,jpb_id INT NOT NULL,salary INT NOT NULL,thoughts NVARCHAR(256) NOT NULL,selection_id INT NOT NULL,user_id INT NOT NULL)")
-# cur.execute("CREATE TABLE Selection_detail (id  INT IDENTITY PRIMARY KEY,sum_output INT NOT NULL,sum_intern INT NOT NULL,prepare_text NVARCHAR(256),skip_presence INT NOT NULL ,skip_detail NVARCHAR(256))")
-# cur.execute("CREATE TABLE Job_type (id  INT IDENTITY PRIMARY KEY,job_name NVARCHAR(128) NOT NULL)")
-# cur.execute("CREATE TABLE Period (id  INT IDENTITY PRIMARY KEY,period INT NOT NULL)")
-# cur.execute("CREATE TABLE Season (id  INT IDENTITY PRIMARY KEY,season_name NVARCHAR(128) NOT NULL)")
+# cur.execute("CREATE TABLE intern_detail(id INT IDENTITY PRIMARY KEY,company_name NVARCHAR(128) NOT NULL,period_id INT NOT NULL,jpb_id INT NOT NULL,salary INT NOT NULL,thoughts NVARCHAR(256) NOT NULL,selection_id INT NOT NULL,user_id INT NOT NULL)")
+# cur.execute("CREATE TABLE selection_detail (id  INT IDENTITY PRIMARY KEY,sum_output INT NOT NULL,sum_intern INT NOT NULL,prepare_text NVARCHAR(256),skip_presence INT NOT NULL ,skip_detail NVARCHAR(256))")
+# cur.execute("CREATE TABLE job_type (id  INT IDENTITY PRIMARY KEY,job_name NVARCHAR(128) NOT NULL)")
+# cur.execute("CREATE TABLE period (id  INT IDENTITY PRIMARY KEY,period_text INT NOT NULL)")
+# cur.execute("CREATE TABLE season (id  INT IDENTITY PRIMARY KEY,season_name NVARCHAR(128) NOT NULL)")
 
 print("OK")
 cur.commit()
