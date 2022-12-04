8d2813bf98ef11466f22770759fa14406dbca7dd
ltoppyl <toppyajibutu@gmail.com>
2022-12-04 13:40:24 +0900

    feat: ヘッダーコンポーネントの作成

---
+++ frontend/components/Header/Header.stories.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { Header } from "./Header";
+
+export default {
+  title: "Header",
+  component: Header,
+} as ComponentMeta<typeof Header>;
+
+const Template: ComponentStory<typeof Header> = (args) => <Header {...args} />;
+
+export const Sample = Template.bind({});
+Sample.args = {
+  isLogin: false,
+};
---
+++ frontend/components/Header/Header.tsx
@@ -0,0 +1,118 @@
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  HStack,
+  VStack,
+  Image,
+  Text,
+  Button,
+  Avatar,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverBody,
+  PopoverArrow,
+  PopoverCloseButton,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+import { FcGoogle } from "react-icons/fc";
+import { PlusSquareIcon } from "@chakra-ui/icons";
+
+type Props = {
+  isLogin: boolean;
+};
+
+type Size = {
+  imageSize: number;
+  stackSizeLarge: string;
+  stackSizeSmall: string;
+  headerTextSize: string;
+  loginButtonSize: string;
+  avatarSize: string;
+};
+export const Header = ({ isLogin }: Props) => {
+  const mediaType = useBreakpointValue({ base: "phone", md: "pc" });
+  const [size, setSize] = useState<Size>({
+    imageSize: 12,
+    stackSizeLarge: "90%",
+    stackSizeSmall: "10%",
+    headerTextSize: "xl",
+    loginButtonSize: "md",
+    avatarSize: "md",
+  });
+  useEffect(() => {
+    if (mediaType === "phone") {
+      setSize({
+        imageSize: 8,
+        stackSizeLarge: "70%",
+        stackSizeSmall: "30%",
+        headerTextSize: "md",
+        loginButtonSize: "sm",
+        avatarSize: "sm",
+      });
+    } else if (mediaType === "pc") {
+      setSize({
+        imageSize: 12,
+        stackSizeLarge: "90%",
+        stackSizeSmall: "10%",
+        headerTextSize: "xl",
+        loginButtonSize: "md",
+        avatarSize: "md",
+      });
+    }
+  }, [mediaType]);
+
+  return (
+    <>
+      <Box bg="blue.100" p={2}>
+        <HStack>
+          <HStack w={size.stackSizeLarge}>
+            <Image
+              boxSize={size.imageSize}
+              // NOTE: ログイン時の画像 URL は isLogin に応じてこのファイルで取得する(Recoil を使用する予定)
+              src="https://bit.ly/dan-abramov"
+              alt="logo"
+            />
+            {/* TODO: サービス名を入れる */}
+            <Text fontSize={size.headerTextSize}>すごいヘッダー</Text>
+          </HStack>
+          {isLogin ? (
+            <HStack w={size.stackSizeSmall}>
+              <PlusSquareIcon boxSize={size.imageSize} />
+              <Popover>
+                <PopoverTrigger>
+                  {/* TODO: _hover を作用させる */}
+                  <Avatar
+                    size={size.avatarSize}
+                    name="###"
+                    src="https://bit.ly/dan-abramov"
+                  />
+                </PopoverTrigger>
+                <PopoverContent p={0}>
+                  <PopoverArrow />
+                  <PopoverCloseButton />
+                  <PopoverBody>
+                    <VStack>
+                      <Button variant="ghost">投稿情報</Button>
+                      <Button variant="ghost">ログアウト</Button>
+                    </VStack>
+                  </PopoverBody>
+                </PopoverContent>
+              </Popover>
+            </HStack>
+          ) : (
+            <Button
+              w={size.stackSizeSmall}
+              leftIcon={<FcGoogle />}
+              borderColor="white"
+              variant="outline"
+              size={size.loginButtonSize}
+            >
+              ログイン
+            </Button>
+          )}
+        </HStack>
+      </Box>
+    </>
+  );
+};
