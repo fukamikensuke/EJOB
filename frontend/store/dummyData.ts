8588fc4b9e8bf70545bc92922233ca617c2568ee
ltoppyl <toppyajibutu@gmail.com>
2022-12-04 16:20:04 +0900

    docs: ダミーデータの追加

--- frontend/store/dummyData.ts
+++ frontend/store/dummyData.ts
@@ -41,3 +41,63 @@ export const filterData = {
     salary: [], // 選択としないため null で良い
   },
 };
+
+export const inputFormData = {
+  evaluation: [
+    { id: 1, text: "1" },
+    { id: 2, text: "2" },
+    { id: 3, text: "3" },
+    { id: 4, text: "4" },
+    { id: 5, text: "5" },
+  ],
+  year: [
+    { id: 1, text: "2020年" },
+    { id: 2, text: "2021年" },
+    { id: 3, text: "2022年" },
+  ],
+  internType: [
+    { id: 1, text: "サマーインターン" },
+    { id: 2, text: "ウィンターインターン" },
+    { id: 3, text: "通年インターン" },
+    { id: 4, text: "その他" },
+  ],
+  period: [
+    { id: 1, text: "1日" },
+    { id: 2, text: "2日" },
+    { id: 3, text: "3日" },
+    { id: 4, text: "4日" },
+    { id: 5, text: "5日" },
+    { id: 6, text: "1週間" },
+    { id: 7, text: "2週間" },
+    { id: 8, text: "3週間" },
+    { id: 9, text: "4週間" },
+    { id: 10, text: "それ以上" },
+  ],
+  jobType: [
+    { id: 1, text: "SE" },
+    { id: 2, text: "Web エンジニア" },
+    { id: 3, text: "ネイティブアプリエンジニア" },
+    { id: 4, text: "フロントエンドエンジニア" },
+    { id: 5, text: "バックエンドエンジニア" },
+    { id: 6, text: "インフラエンジニア" },
+    { id: 7, text: "フルスタックエンジニア" },
+    { id: 8, text: "組み込みソフトウェアエンジニア" },
+    { id: 9, text: "機械学習エンジニア" },
+    { id: 10, text: "データサイエンティスト" },
+    { id: 10, text: "その他" },
+  ],
+  developEx: [
+    { id: 1, text: "0回" },
+    { id: 2, text: "1回" },
+    { id: 3, text: "2回" },
+    { id: 4, text: "3回" },
+    { id: 5, text: "4回以上" },
+  ],
+  internEx: [
+    { id: 1, text: "0回" },
+    { id: 2, text: "1回" },
+    { id: 3, text: "2回" },
+    { id: 4, text: "3回" },
+    { id: 5, text: "4回以上" },
+  ],
+};
