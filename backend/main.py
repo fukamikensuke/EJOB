3736a7e43cd0e2571c55fcf734a20749dae44448
Kensuke Fukami <kensuke8293@gmail.com>
2022-12-04 12:06:01 +0900

    fix: format

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,7 @@ def get_select_filed():
     out_put = {"data" :{"year":{"id":1,"text":"サマーインターン"},"internType":{"id":1,"text":"サマーインターン"},"period":{"id":1,"text":"2週間"},"jobType":{"id":1,"text":"フロントエンジニア"},"developEx":{"id":1,"text":"０回"},"internEx":{"id":1,"text":"0回"}}}
     return out_put
 
-@app.post("/user-info")
+@app.post("/intern-info")
 def post_intern_info(
     company : str = Form(),
     year : int = Form(),
@@ -32,7 +32,7 @@ def post_intern_info(
     period : int = Form(),
     jobType : int = Form(),
     salary : int = Form(),
-    internContens : str = Form(),
+    internContents : str = Form(),
     evaluation : int = Form(),
     developEx : int = Form(),
     internEx : int = Form(),
