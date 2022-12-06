b59c2b73ba089c3276acac82c207735378ed334d
Kensuke Fukami <kensuke8293@gmail.com>
2022-12-06 15:20:16 +0900

    feat :delete

---
+++ backend/cruds/delete.py
@@ -0,0 +1,23 @@
+import os
+import pyodbc
+from dotenv import load_dotenv
+
+
+def delete_intern_info(env_list: list, id: int):
+    cnxn = pyodbc.connect(
+        "DRIVER="
+        + env_list[4]
+        + ";SERVER="
+        + env_list[0]
+        + ";DATABASE="
+        + env_list[1]
+        + ";UID="
+        + env_list[2]
+        + ";PWD="
+        + env_list[3]
+    )
+    cur = cnxn.cursor()
+    cur.execute("DELETE FROM intern_detail WHERE id = %d" % (id))
+
+    cur.commit()
+    return "OK"
