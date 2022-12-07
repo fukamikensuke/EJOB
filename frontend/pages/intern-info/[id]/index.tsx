a1d6f2ac02ff182823864b37134b10d5808ec592
ltoppyl <toppyajibutu@gmail.com>
2022-12-07 20:32:40 +0900

    fix: フォルダー名の修正
    一般的な命名規則(アッパーキャメルケース)に従うため

--- frontend/pages/intern-info/[id]/index.tsx
+++ frontend/pages/intern-info/[id]/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useRouter } from "next/router";
 import { Container } from "@chakra-ui/react";
-import { InternInfo } from "../../../components/InternIfnoTemp/InternInfo";
+import { InternInfo } from "../../../components/InternInfo/InternInfo";
 import { Header } from "../../../components/Header/Header";
 export default function DisplayInternInfo() {
   const router = useRouter();
--- frontend/pages/post.tsx
+++ frontend/pages/post.tsx
@@ -1,7 +1,7 @@
 import { Container } from "@chakra-ui/react";
 import React from "react";
 import { Header } from "../components/Header/Header";
-import { InputForm } from "../components/InputFormTem/InputForm";
+import { InputForm } from "../components/InputForm/InputForm";
 export default function Post() {
   return (
     <>
