a52022297e605c5afe69b1103fae0bfb38576a1c
Kensuke Fukami <kensuke8293@gmail.com>
2022-12-08 17:46:54 +0900

    fix : DBバグ発動

--- backend/cruds/intern_get_list.py
+++ backend/cruds/intern_get_list.py
@@ -18,22 +18,35 @@ def get_intern_info(env_list: list):
         + env_list[3]
     )
     cur = cnxn.cursor()
+    _cur = cnxn.cursor()
     cur.execute("SELECT * FROM intern_detail")
     output_data = []
     for row in cur:
         print(row[0], row[1])
+        # _cur.execute("SELECT text FROM year WHERE id = %d" % row[2])
+        # (year_data,) = _cur.fetchone()
+        # _cur.execute("SELECT job_name FROM job_type WHERE id = %d" % row[5])
+        # (job_data,) = _cur.fetchone()
+        # _cur.execute("SELECT season_name FROM season WHERE id = %d" % row[3])
+        # (season_data,) = _cur.fetchone()
+        # _cur.execute("SELECT period_text FROM period WHERE id = %d" % row[4])
+        # (period_data,) = _cur.fetchone()
+        # _cur.execute("SELECT text FROM evaluation WHERE id = %d" % row[8])
+        # (evaluation_data,) = _cur.fetchone()
         output_data.append(
             {
                 "id": row[0],
                 "companyName": row[1],
-                "evaluation": row[8],
-                "year": row[2],
-                "internType": row[3],
-                "period": row[4],
-                "job": row[5],
+                "evaluation": row[8],  # evaluation_data,
+                "year": row[2],  # year_data,
+                "internType": row[3],  # season_data,
+                "period": row[4],  # period_data,
+                "job": row[5],  # job_data,
                 "salary": row[6],
             }
         )
     # TODO ここ index じゃなくて、カラム名とかからデータとれないのだろうか！？(取れそう)
     cur.commit()
+    _cur.commit()
+    cnxn.close()
     return output_data
