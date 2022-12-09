3111ecad6747fe3ab44d592282a20d64e65a3692
ltoppyl <toppyajibutu@gmail.com>
2022-12-09 11:23:44 +0900

    feat: ユーザーの投稿情報ページへのアクセス制御

--- frontend/pages/user/[id]/index.tsx
+++ frontend/pages/user/[id]/index.tsx
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/router";
+import { useRecoilValue } from "recoil";
 import { Text, Center } from "@chakra-ui/react";
+import { loginState } from "../../../store/Recoil";
 import { UserTable } from "../../../components/Table/UserTable";
 import { Header } from "../../../components/Header/Header";
 import { VSpacer } from "../../../components/Spacer/Spacer";
 
 export default function UserInfo() {
   const router = useRouter();
+  const loginStatus = useRecoilValue(loginState);
   const { id } = router.query || "";
+
+  useEffect(() => {
+    if (id !== loginStatus.uid) {
+      router.push("/error");
+    }
+  }, [id, loginStatus.uid, router]);
 
   return (
     <>
       <Header />
       <Center>
-        <Text fontSize="3xl">userid {id} さんの投稿したインターン情報</Text>
+        <Text fontSize="3xl">
+          {loginStatus.name} さんの投稿したインターン情報
+        </Text>
       </Center>
       <VSpacer size={10} />
       <UserTable />
