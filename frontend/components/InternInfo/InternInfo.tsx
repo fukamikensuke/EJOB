458f84da1b0095c0ee42c20531f19108f0d5fd06
ltoppyl <toppyajibutu@gmail.com>
2022-12-10 16:39:29 +0900

    feat: 詳細の情報を API から取得してきたものに変更

--- frontend/components/InternInfo/InternInfo.tsx
+++ frontend/components/InternInfo/InternInfo.tsx
@@ -1,14 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { HStack, Text } from "@chakra-ui/react";
 import { VSpacer } from "../Spacer/Spacer";
-import { displayInternInfo } from "../../store/dummyData";
 import { CustomText } from "./CustomText";
+import axios, { AxiosRequestConfig } from "axios";
 
-export const InternInfo = () => {
+type Props = {
+  id: string | string[] | undefined;
+};
+
+export const InternInfo = ({ id }: Props) => {
+  const [displayInternInfoAPI, setDisplayInternInfoAPI] = useState({
+    companyName: "",
+    year: "",
+    internType: "",
+    period: "",
+    job: "",
+    salary: "",
+    internContents: "",
+    evaluation: "",
+    developEx: "",
+    internEx: "",
+    internTestPreparation: "",
+    isSelectionExemption: "",
+    selectionExemptionContents: "",
+    impressions: "",
+  });
+
   const WLarge = "85%";
   const WSmall = "15%";
   const spaceBetweenItems = 4;
   const spaceBetweenTitle = 16;
+
+  useEffect(() => {
+    const baseUrl = `http://localhost:8000/intern-info/${id}`;
+
+    const options: AxiosRequestConfig = {
+      url: `${baseUrl}`,
+      method: "GET",
+    };
+
+    axios(options)
+      .then((res) => {
+        console.log(res.data);
+        setDisplayInternInfoAPI(res.data.data);
+      })
+      .catch((error) => {
+        // eslint-disable-next-line no-console
+        console.log("error of GET/intern-info", error);
+      });
+  }, []);
 
   return (
     <>
@@ -17,37 +57,37 @@ export const InternInfo = () => {
       <VSpacer size={spaceBetweenItems} />
       <HStack>
         <Text w={WSmall}>企業名</Text>
-        <CustomText width={WLarge} text={displayInternInfo.company} />
+        <CustomText width={WLarge} text={displayInternInfoAPI.companyName} />
       </HStack>
       <VSpacer size={spaceBetweenItems} />
       <HStack>
         <Text w={WSmall}>参加年度</Text>
-        <CustomText width={WLarge} text={displayInternInfo.period} />
+        <CustomText width={WLarge} text={displayInternInfoAPI.period} />
       </HStack>
       <VSpacer size={spaceBetweenItems} />
       <HStack>
         <Text w={WSmall}>インターン種別</Text>
-        <CustomText width={WLarge} text={displayInternInfo.internType} />
+        <CustomText width={WLarge} text={displayInternInfoAPI.internType} />
       </HStack>
       <VSpacer size={spaceBetweenItems} />
       <HStack>
         <Text w={WSmall}>期間</Text>
-        <CustomText width={WLarge} text={displayInternInfo.period} />
+        <CustomText width={WLarge} text={displayInternInfoAPI.period} />
       </HStack>
       <VSpacer size={spaceBetweenItems} />
       <HStack>
         <Text w={WSmall}>職種</Text>
-        <CustomText width={WLarge} text={displayInternInfo.jobType} />
+        <CustomText width={WLarge} text={displayInternInfoAPI.job} />
       </HStack>
       <VSpacer size={spaceBetweenItems} />
       <HStack>
         <Text w={WSmall}>報酬(時給)</Text>
-        <CustomText width={WLarge} text={displayInternInfo.salary} />
+        <CustomText width={WLarge} text={displayInternInfoAPI.salary} />
       </HStack>
       <VSpacer size={spaceBetweenItems} />
       <HStack>
         <Text w={WSmall}>インターンの内容</Text>
-        <CustomText width={WLarge} text={displayInternInfo.internContents} />
+        <CustomText width={WLarge} text={displayInternInfoAPI.internContents} />
       </HStack>
 
       <VSpacer size={spaceBetweenTitle} />
@@ -57,29 +97,29 @@ export const InternInfo = () => {
       <VSpacer size={spaceBetweenItems} />
       <HStack>
         <Text w={"35%"}>選考時の趣味開発やハッカソン等での開発経験</Text>
-        <CustomText width={"65%"} text={displayInternInfo.developEx} />
+        <CustomText width={"65%"} text={displayInternInfoAPI.developEx} />
       </HStack>
       <VSpacer size={spaceBetweenItems} />
       <HStack>
         <Text w={"30%"}>選考時のインターンへの参加経験</Text>
-        <CustomText width={"70%"} text={displayInternInfo.internEx} />
+        <CustomText width={"70%"} text={displayInternInfoAPI.internEx} />
       </HStack>
       <VSpacer size={spaceBetweenItems} />
       <Text>インターンの選考対策として行ったことがあれば教えてください</Text>
       <VSpacer size={spaceBetweenItems} />
-      <CustomText text={displayInternInfo.internTestPreparation} />
+      <CustomText text={displayInternInfoAPI.internTestPreparation} />
       <VSpacer size={spaceBetweenItems} />
       <Text>インターンに参加したことでその先の選考の免除があったか</Text>
       <VSpacer size={spaceBetweenItems} />
-      <CustomText text={displayInternInfo.isSelectionExemption} />
+      <CustomText text={displayInternInfoAPI.isSelectionExemption} />
       <VSpacer size={spaceBetweenItems} />
       <Text>免除の内容</Text>
       <VSpacer size={spaceBetweenItems} />
-      <CustomText text={displayInternInfo.selectionExemptionContents} />
+      <CustomText text={displayInternInfoAPI.selectionExemptionContents} />
       <VSpacer size={spaceBetweenItems} />
       <Text>全体の感想(参加して良かったことや改善点など)</Text>
       <VSpacer size={spaceBetweenItems} />
-      <CustomText text={displayInternInfo.impressions} />
+      <CustomText text={displayInternInfoAPI.impressions} />
     </>
   );
 };
