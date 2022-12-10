6073e9de5c685d6ed9141aa1d4c4e2e2cd781cd2
ltoppyl <toppyajibutu@gmail.com>
2022-12-10 14:07:55 +0900

    feat: 特定の id のインターン情報の取得

--- backend/cruds/intern_get_detail.py
+++ backend/cruds/intern_get_detail.py
@@ -17,8 +17,25 @@ def get_intern_detail(id: int, env_list: list):
         + env_list[3]
     )
     cur = cnxn.cursor()
+    _cur = cnxn.cursor()
     cur.execute("SELECT * FROM intern_detail WHERE id = %d" % id)
+
     for row in cur:
+        _cur.execute("SELECT text FROM year WHERE id = %d" % row[2])
+        (year_data,) = _cur.fetchone()
+        _cur.execute("SELECT job_name FROM job_type WHERE id = %d" % row[5])
+        (job_data,) = _cur.fetchone()
+        _cur.execute("SELECT season_name FROM season WHERE id = %d" % row[3])
+        (season_data,) = _cur.fetchone()
+        _cur.execute("SELECT period_text FROM period WHERE id = %d" % row[4])
+        (period_data,) = _cur.fetchone()
+        _cur.execute("SELECT text FROM evaluation WHERE id = %d" % row[8])
+        (evaluation_data,) = _cur.fetchone()
+        _cur.execute("SELECT text FROM develop_ex WHERE id = %d" % row[10])
+        (develop_data,) = _cur.fetchone()
+        _cur.execute("SELECT text FROM intern_ex WHERE id = %d" % row[11])
+        (intern_data,) = _cur.fetchone()
+        (year_data)
         output_data = {
             "companyName": row[1],
             "year": row[2],
@@ -36,4 +53,6 @@ def get_intern_detail(id: int, env_list: list):
             "impressions": row[9],
         }
     cur.commit()
+    _cur.commit()
+    cnxn.close()
     return output_data
