3afbd1aea4665d14d8acac8adb15e8e9e8a45c41
ltoppyl <toppyajibutu@gmail.com>
2022-12-11 11:04:38 +0900

    refactor: ヘッダーを共通で使用

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,11 +1,13 @@
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Header } from "../components/Header/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <ChakraProvider>
+        <Header />
         <Component {...pageProps} />
       </ChakraProvider>
     </RecoilRoot>
